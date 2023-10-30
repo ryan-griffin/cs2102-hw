d81bb9ac4fba33afa15c7ad907e83ea3bb5e5512
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-10-30 17:44:27 -0400

    update hw1

--- hw1/PetRescue.java
+++ hw1/PetRescue.java
@@ -1,36 +1,97 @@
 import java.util.LinkedList;
 
 public class PetRescue implements PetRescueable {
+    public LinkedList<Integer> birdWeights;
+    public LinkedList<Integer> dogYears;
     public String petOfTheMonth;
+    public int pellets = 0;
+    public int hay = 0;
+    public LinkedList<Coord> catCoords;
 
     public PetRescue(LinkedList<Integer> birdWeights,
             LinkedList<Integer> dogYears,
             String petOfTheMonth,
             LinkedList<Coord> catCoords) {
+        this.birdWeights = birdWeights;
+        this.dogYears = dogYears;
+        this.petOfTheMonth = petOfTheMonth;
+        this.catCoords = catCoords;
     }
 
-    @Override
     public int biggestBird() {
-        throw new UnsupportedOperationException("Unimplemented method 'biggestBird'");
+        if (birdWeights.isEmpty()) {
+            return 0;
+        } else {
+            int maxWeight = birdWeights.getFirst();
+            for (int weight : birdWeights) {
+                if (weight > maxWeight) {
+                    maxWeight = weight;
+                }
+            }
+            return maxWeight;
+        }
     }
 
-    @Override
     public LinkedList<Integer> inHumanYears() {
-        throw new UnsupportedOperationException("Unimplemented method 'inHumanYears'");
+        LinkedList<Integer> humanYearsList = new LinkedList<>();
+
+        for (Integer dogAge : dogYears) {
+            int humanAge = dogAge * 7;
+            humanYearsList.add(humanAge);
+        }
+
+        return humanYearsList;
     }
 
-    @Override
     public void bestowHonor(String title, String credential) {
-        throw new UnsupportedOperationException("Unimplemented method 'bestowHonor'");
+        if (!title.isEmpty()) {
+            petOfTheMonth = title + " " + petOfTheMonth;
+        }
+        if (!credential.isEmpty()) {
+            if (petOfTheMonth.contains(",")) {
+                petOfTheMonth += " " + credential;
+            } else {
+                petOfTheMonth += ", " + credential;
+            }
+        }
     }
 
-    @Override
-    public String feedChinchillas(int pellets, int hay) {
-        throw new UnsupportedOperationException("Unimplemented method 'feedChinchillas'");
+    public String feedChinchillas(int pelletsToAdd, int hayToAdd) {
+        pellets += pelletsToAdd;
+        hay += hayToAdd;
+
+        String label = "Chinchilla: " + (pellets >= 0 ? pellets : "unknown") + " pellets, " +
+                (hay >= 0 ? hay : "unknown") + " hay";
+
+        if (pellets < 0) {
+            pellets = 0;
+        }
+        if (hay < 0) {
+            hay = 0;
+        }
+
+        return label;
     }
 
-    @Override
     public String closestTo(int x, int y) {
-        throw new UnsupportedOperationException("Unimplemented method 'closestTo'");
+        if (catCoords.isEmpty()) {
+            return "Conspiciously Catless";
+        } else {
+            String closestCat = null;
+            double minDistance = Double.MAX_VALUE;
+
+            for (Coord catCoord : catCoords) {
+                int catX = catCoord.x;
+                int catY = catCoord.y;
+                double distance = Math.sqrt((x - catX) * (x - catX) + (y - catY) * (y - catY));
+
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestCat = catCoord.name;
+                }
+            }
+
+            return closestCat;
+        }
     }
 }
