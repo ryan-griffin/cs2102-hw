2e4732512d6748cde1c98f73f1d131e627c064b3
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-10-30 19:02:14 -0400

    finish hw1

--- hw1/Examples.java
+++ hw1/Examples.java
@@ -3,9 +3,11 @@ import static org.junit.Assert.*;
 import java.util.LinkedList;
 
 public class Examples {
-
-	PetRescue pr;
 	LinkedList<Integer> someBirdWeights = new LinkedList<Integer>();
+	LinkedList<Integer> someDogYears = new LinkedList<Integer>();
+	String somePetOfTheMonth = new String();
+	LinkedList<Coord> someCatCoords = new LinkedList<Coord>();
+	PetRescue pr = new PetRescue(someBirdWeights, someDogYears, somePetOfTheMonth, someCatCoords);
 
 	public Examples() {
 		someBirdWeights.add(10); // 10oz
@@ -13,55 +15,60 @@ public class Examples {
 
 	@Test
 	public void testDogYearsNonEmpty() {
-		LinkedList<Integer> birdWeight = new LinkedList<>();
-		LinkedList<Integer> dogYears = new LinkedList<>();
-		LinkedList<Coord> catCords = new LinkedList<>();
-		dogYears.add(2);
-		dogYears.add(3);
-		pr = new PetRescue(birdWeight, dogYears, "", catCords);
+		someDogYears.add(2);
+		someDogYears.add(3);
 
 		LinkedList<Integer> someHumanYears = new LinkedList<>();
 		someHumanYears.add(14);
 		someHumanYears.add(21);
-		assertEquals("Dog year in human years should be 14 and 21 respectively.", someHumanYears, pr.inHumanYears());
+		assertEquals(someHumanYears, pr.inHumanYears());
 	}
 
 	@Test
 	public void testBirdWeightsNonEmpty() {
-		pr = new PetRescue(someBirdWeights, new LinkedList<Integer>(), "", new LinkedList<Coord>());
-		assertEquals("10 should be the biggest bird out of 1 bird", 10, pr.biggestBird()); // label text (optional),
-																							// expected value,
-																							// actual/check value
+		assertEquals(10, pr.biggestBird()); // label text (optional),
+											// expected value,
+											// actual/check value
 	}
 
 	@Test
 	public void testBestowHonor() {
-		pr = new PetRescue(someBirdWeights, new LinkedList<Integer>(), "", new LinkedList<Coord>());
 		pr.bestowHonor("Dr.", "PhD");
 		assertEquals("Dr. , PhD", pr.petOfTheMonth);
 	}
 
 	@Test
+	public void testBestowHonor2() {
+		pr.bestowHonor("Dr.", "PhD");
+		pr.bestowHonor("Capt.", "Esq.");
+		assertEquals("Capt. Dr. , PhD Esq.", pr.petOfTheMonth);
+	}
+
+	@Test
 	public void testFeedChinchillasZero() {
-		pr = new PetRescue(someBirdWeights, new LinkedList<Integer>(), "", new LinkedList<Coord>());
 		assertEquals("Chinchilla: 0 pellets, 0 hay", pr.feedChinchillas(0, 0));
 	}
 
 	@Test
 	public void testFeedChinchillasPositive() {
-		pr = new PetRescue(someBirdWeights, new LinkedList<Integer>(), "", new LinkedList<Coord>());
 		assertEquals("Chinchilla: 5 pellets, 10 hay", pr.feedChinchillas(5, 10));
 	}
 
 	@Test
 	public void testFeedChinchillasNegative() {
-		pr = new PetRescue(someBirdWeights, new LinkedList<Integer>(), "", new LinkedList<Coord>());
 		assertEquals("Chinchilla: unknown pellets, unknown hay", pr.feedChinchillas(-5, -10));
 	}
 
 	@Test
 	public void testClosestToEmpty() {
-		pr = new PetRescue(someBirdWeights, new LinkedList<Integer>(), "", new LinkedList<Coord>());
 		assertEquals("Conspiciously Catless", pr.closestTo(0, 0));
+	}
+
+	@Test
+	public void testClosestToNonEmpty() {
+		someCatCoords.add(new Coord("Cat1", 0, 0));
+		someCatCoords.add(new Coord("Cat2", 1, 1));
+		someCatCoords.add(new Coord("Cat3", 2, 2));
+		assertEquals("Cat1", pr.closestTo(0, 0));
 	}
 }
--- hw1/PetRescue.java
+++ hw1/PetRescue.java
@@ -1,6 +1,19 @@
 import java.util.LinkedList;
 
+/**
+ * PetRescue is a class that implements the PetRescueable interface.
+ * It contains methods that perform operations on the data stored in
+ * the instance variables.
+ */
 public class PetRescue implements PetRescueable {
+    /**
+     * birdWeights: a list of bird weights in oz
+     * dogYears: a list of dog ages in years
+     * petOfTheMonth: the name of the pet of the month
+     * pellets: the amount of pellets in the pantry
+     * hay: the amount of hay in the pantry
+     * catCoords: a list of cat coordinates
+     */
     public LinkedList<Integer> birdWeights;
     public LinkedList<Integer> dogYears;
     public String petOfTheMonth;
@@ -8,6 +21,14 @@ public class PetRescue implements PetRescueable {
     public int hay = 0;
     public LinkedList<Coord> catCoords;
 
+    /**
+     * Constructor for PetRescue
+     *
+     * @param birdWeights   a list of bird weights in oz
+     * @param dogYears      a list of dog ages in years
+     * @param petOfTheMonth the name of the pet of the month
+     * @param catCoords     a list of cat coordinates
+     */
     public PetRescue(LinkedList<Integer> birdWeights,
             LinkedList<Integer> dogYears,
             String petOfTheMonth,
@@ -18,6 +39,11 @@ public class PetRescue implements PetRescueable {
         this.catCoords = catCoords;
     }
 
+    /**
+     * Searches the birds in the pet rescue
+     *
+     * @return the size of the biggest bird in oz or 0 if there are no birds
+     */
     public int biggestBird() {
         if (birdWeights.isEmpty()) {
             return 0;
@@ -32,6 +58,12 @@ public class PetRescue implements PetRescueable {
         }
     }
 
+    /**
+     * Transforms the dog records in the pet rescue
+     *
+     * @return a list of ages in the same order as the dog records,
+     *         but each age is converted into human years by multiplying by 7
+     */
     public LinkedList<Integer> inHumanYears() {
         LinkedList<Integer> humanYearsList = new LinkedList<>();
 
@@ -43,6 +75,22 @@ public class PetRescue implements PetRescueable {
         return humanYearsList;
     }
 
+    /**
+     * Awards the pet of the month with a title and/or a credential for their
+     * accomplishments
+     *
+     * @param title      Can be a title like "Dr." or "Capt."
+     *                   or the empty string "" if no title to be added
+     * @param credential a degree or honor including but not limited to "MD", "PhD",
+     *                   or "Esq."
+     *                   or the empty string "" if no credential to be added
+     *                   effects: modifies the name on record of the form "TITLE
+     *                   name, CREDENTIAL"
+     *                   multiple space separated titles can be added with the
+     *                   newest first
+     *                   multiple space separated credentials can be added after the
+     *                   comma with the newest last.
+     */
     public void bestowHonor(String title, String credential) {
         if (!title.isEmpty()) {
             petOfTheMonth = title + " " + petOfTheMonth;
@@ -56,6 +104,17 @@ public class PetRescue implements PetRescueable {
         }
     }
 
+    /**
+     * Adds positive or subtracts negative food from the pantry and then prints out
+     * a new label for the contents inside
+     * assume the initial amount of pellets and hay should be 0
+     *
+     * @param pellets the amount of pellets to add/subtract
+     * @param hay     the amount of hay to add/subtract
+     * @return a string in the format "Chinchilla: # pellets, # hay"
+     *         If the storage of pellets or hay goes negative, reset them to 0, and
+     *         replace # with "unknown"
+     */
     public String feedChinchillas(int pelletsToAdd, int hayToAdd) {
         pellets += pelletsToAdd;
         hay += hayToAdd;
@@ -73,6 +132,14 @@ public class PetRescue implements PetRescueable {
         return label;
     }
 
+    /**
+     * Finds the closest cat in the rescue to some coordinate
+     *
+     * @param x the coordinate to find the closest to
+     * @param y
+     * @return the name of the cat if found or "Conspiciously Catless" if there are
+     *         no cats.
+     */
     public String closestTo(int x, int y) {
         if (catCoords.isEmpty()) {
             return "Conspiciously Catless";
