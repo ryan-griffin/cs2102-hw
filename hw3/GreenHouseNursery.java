29df285bf68e357543675378f9e90fadbff5cea4
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-11-14 22:40:42 -0500

    GreenHouse update

--- hw3/GreenHouseNursery.java
+++ hw3/GreenHouseNursery.java
@@ -1,21 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class GreenHouseNursery extends AbsGreenHouse implements Sensible {
-    public GreenHouseNursery() {
-    }
+    public HashMap<Double, List<Double>> data = new HashMap<Double, List<Double>>();
 
     @Override
     public void pollSensorData(List<Double> values) {
+        if (values.isEmpty()) {
+            return;
+        }
 
+        for (int i = 0; i < values.size(); i += 5) {
+            double date = values.get(i);
+            double temp1 = values.get(i + 1);
+            double humid1 = values.get(i + 2);
+            double temp2 = values.get(i + 3);
+            double humid2 = values.get(i + 4);
+
+            List<Double> readings = new ArrayList<Double>();
+            readings.add(temp1);
+            readings.add(humid1);
+            readings.add(temp2);
+            readings.add(humid2);
+
+            data.put(date, readings);
+        }
     }
 
     @Override
     public TempHumidReading middleReading() {
-        return null;
+        if (data.isEmpty()) {
+            return new TempHumidReading(-999.0, -999.0);
+        }
+
+        double totalTemp = 0;
+        double totalHumidity = 0;
+        int count = 0;
+
+        for (List<Double> readings : data.values()) {
+            for (int i = 0; i < readings.size(); i += 2) {
+                totalTemp += readings.get(i);
+                totalHumidity += readings.get(i + 1);
+                count++;
+            }
+        }
+
+        double overallAvgTemp = totalTemp / count;
+        double overallAvgHumidity = totalHumidity / count;
+
+        return new TempHumidReading(overallAvgTemp, overallAvgHumidity);
     }
 
     @Override
     public TempHumidReading middleReading(double onDate) {
-        return null;
+        if (data.isEmpty() || !data.containsKey(onDate)) {
+            return new TempHumidReading(-999.0, -999.0);
+        }
+
+        List<Double> readings = data.get(onDate);
+        double totalTemp = 0;
+        double totalHumidity = 0;
+
+        for (int i = 0; i < readings.size(); i += 2) {
+            totalTemp += readings.get(i);
+            totalHumidity += readings.get(i + 1);
+        }
+
+        double avgTemp = totalTemp / (readings.size() / 2);
+        double avgHumidity = totalHumidity / (readings.size() / 2);
+
+        return new TempHumidReading(avgTemp, avgHumidity);
     }
 }
--- hw3/GreenHouseProduce.java
+++ hw3/GreenHouseProduce.java
@@ -1,21 +1,80 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GreenHouseProduce extends AbsGreenHouse implements Sensible {
-    public GreenHouseProduce() {
-    }
+    public HashMap<Double, List<Double>> data = new HashMap<Double, List<Double>>();
 
     @Override
     public void pollSensorData(List<Double> values) {
+        if (values.isEmpty()) {
+            return;
+        }
 
+        for (int i = 0; i < values.size(); i += 5) {
+            double date = values.get(i);
+            double temp1 = values.get(i + 1);
+            double humid1 = values.get(i + 2);
+            double temp2 = values.get(i + 3);
+            double humid2 = values.get(i + 4);
+
+            double avgTemp = (temp1 + temp2) / 2;
+            double avgHumidity = (humid1 + humid2) / 2;
+
+            if (!data.containsKey(date)) {
+                data.put(date, new ArrayList<Double>());
+            }
+
+            data.get(date).add(avgTemp);
+            data.get(date).add(avgHumidity);
+        }
     }
 
     @Override
     public TempHumidReading middleReading() {
-        return null;
+        if (data.isEmpty()) {
+            return new TempHumidReading(-999.0, -999.0);
+        }
+
+        double totalAvgTemp = 0;
+        double totalAvgHumidity = 0;
+        int count = 0;
+
+        for (Map.Entry<Double, List<Double>> entry : data.entrySet()) {
+            List<Double> readings = entry.getValue();
+
+            for (int i = 0; i < readings.size(); i += 2) {
+                totalAvgTemp += readings.get(i);
+                totalAvgHumidity += readings.get(i + 1);
+                count++;
+            }
+        }
+
+        double overallAvgTemp = totalAvgTemp / count;
+        double overallAvgHumidity = totalAvgHumidity / count;
+
+        return new TempHumidReading(overallAvgTemp, overallAvgHumidity);
     }
 
     @Override
     public TempHumidReading middleReading(double onDate) {
-        return null;
+        if (data.isEmpty() || !data.containsKey(onDate)) {
+            return new TempHumidReading(-999.0, -999.0);
+        }
+
+        List<Double> readings = data.get(onDate);
+        double totalAvgTemp = 0;
+        double totalAvgHumidity = 0;
+
+        for (int i = 0; i < readings.size(); i += 2) {
+            totalAvgTemp += readings.get(i);
+            totalAvgHumidity += readings.get(i + 1);
+        }
+
+        double overallAvgTemp = totalAvgTemp / (readings.size() / 2);
+        double overallAvgHumidity = totalAvgHumidity / (readings.size() / 2);
+
+        return new TempHumidReading(overallAvgTemp, overallAvgHumidity);
     }
 }
