16768509ce0525ff9d6b0950314053566b6c529f
ryan-griffin <85765087+ryan-griffin@users.noreply.github.com>
2023-12-02 16:19:10 -0500

    finish lab 5

--- hw5/Examples.java
+++ hw5/Examples.java
@@ -3,22 +3,36 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class Examples {
+	MaxHeapValidator validator = new MaxHeapValidator();
 	EmptyBT leaf = new EmptyBT();
 
 	@Test
-	public void testAdd1() {
-		MaxHeapValidator validator = new MaxHeapValidator();
+	public void testAdd() {
 		assertTrue(validator.validAdd(leaf, 1, new NodeBT(1, leaf, leaf)));
 	}
 
 	@Test
-	public void testAddButEmpty() {
-		MaxHeapValidator validator = new MaxHeapValidator();
+	public void testAddEmpty() {
 		assertFalse(validator.validAdd(leaf, 1, leaf));
 	}
 
-	public void testRemoveEmpty() {
-		MaxHeapValidator validator = new MaxHeapValidator();
+	@Test
+	public void testAddFull() {
+		assertFalse(validator.validAdd(new NodeBT(2, leaf, leaf), 1, new NodeBT(1, leaf, leaf)));
+	}
+
+	@Test
+	public void testRemove() {
 		assertTrue(validator.validRemove(new NodeBT(2, leaf, leaf), 2, leaf));
+	}
+
+	@Test
+	public void testRemoveEmpty() {
+		assertFalse(validator.validRemove(leaf, 1, leaf));
+	}
+
+	@Test
+	public void testRemoveFull() {
+		assertFalse(validator.validRemove(new NodeBT(2, leaf, leaf), 1, new NodeBT(1, leaf, leaf)));
 	}
 }
